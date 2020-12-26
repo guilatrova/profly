d340285622e78c4522ae1e3066411993bde2e01a
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-26 11:56:57 -0300

    chore: clean up partially demo app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import configureStore, { history } from './store/configureStore';
-import Root from './components/Root';
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import configureStore, { history } from './core/store/configureStore';
+import Root from './Root';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 const store = configureStore();
 
@@ -17,8 +16,8 @@ render(
 );
 
 if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-    const NewRoot = require('./components/Root').default;
+  module.hot.accept('./Root', () => {
+    const NewRoot = require('./Root').default;
     render(
       <AppContainer>
         <NewRoot store={store} history={history} />
