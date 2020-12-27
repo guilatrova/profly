046096a3e3b2bcfcd6e20eae0fb2e7c0bd06349f
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-27 17:03:11 -0300

    feat: implement action toggle

---
+++ src/transactions/components/StrikeActionToggle.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+
+const useStyles = makeStyles((theme) => ({
+  toggleContainer: {
+    margin: theme.spacing(2, 0),
+  },
+}));
+
+const StrikeActionToggle = ({ onChange }) => {
+  const [action, setAction] = useState('buy');
+
+  const handleChange = (event, newAction) => {
+    if (newAction) {
+      setAction(newAction);
+      onChange(newAction);
+    }
+  };
+
+  const classes = useStyles();
+
+  return (
+    <div className={classes.toggleContainer}>
+      <ToggleButtonGroup
+        value={action}
+        exclusive
+        onChange={handleChange}
+      >
+        <ToggleButton value="buy">
+          Buy
+        </ToggleButton>
+
+        <ToggleButton value="sell">
+          Sell
+        </ToggleButton>
+      </ToggleButtonGroup>
+    </div>
+  );
+}
+
+StrikeActionToggle.propTypes = {
+  onChange: PropTypes.func.isRequired
+}
+
+export default StrikeActionToggle;
