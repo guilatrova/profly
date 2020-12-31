e665c16801d0827a7d91608dd020d78e6a295627
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-31 17:16:50 -0300

    feat: make pie chart responsive

--- src/charts/components/ValueSpreadPieChart.js
+++ src/charts/components/ValueSpreadPieChart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { PieChart, Pie, Cell } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 import { randomColor } from '../../utils/colors';
 import ActiveShape from './ValueSpreadActiveShape';
 
@@ -15,25 +15,25 @@ const ValueSpreadPieChart = ({ chartData }) => {
   }, [chartData]);
 
   return (
-    <PieChart width={500} height={250}>
-      <Pie
-        activeIndex={activeIndex}
-        activeShape={ActiveShape}
-        data={chartData}
-        dataKey="value"
-        cx={150}
-        cy={120}
-        innerRadius={60}
-        outerRadius={80}
-        paddingAngle={5}
-        onMouseEnter={(data, idx) => setActiveIndex(idx)}
-        onMouseLeave={() => setActiveIndex()}
-      >
-        {chartData.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={colors[index]} />
-        ))}
-      </Pie>
-    </PieChart>
+    <ResponsiveContainer width="100%" height="100%" minHeight={250}>
+      <PieChart>
+        <Pie
+          activeIndex={activeIndex}
+          activeShape={ActiveShape}
+          data={chartData}
+          dataKey="value"
+          innerRadius={60}
+          outerRadius={80}
+          paddingAngle={5}
+          onMouseEnter={(data, idx) => setActiveIndex(idx)}
+          onMouseLeave={() => setActiveIndex()}
+        >
+          {chartData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
   );
 };
 
