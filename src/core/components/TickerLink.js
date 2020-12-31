d03f56f9cdf8f3a4b37c45066db83fbe63c0d045
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-30 22:00:56 -0300

    style: add style to ticker links

--- src/core/components/TickerLink.js
+++ src/core/components/TickerLink.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { tickerType } from '../types';
 
+const useStyles = makeStyles((theme) => ({
+  tickerAnchor: {
+    color: theme.palette.secondary.main,
+    textDecoration: 'none',
+    textTransform: 'uppercase',
+    borderBottom: `1px dotted ${theme.palette.secondary.main}`
+  }
+}));
+
 const TickerLink = ({ children: ticker }) => {
+  const classes = useStyles();
+
   return (
     <span>
-      <Link to={`/stocks/${ticker}`}>{ticker}</Link>
+      <Link className={classes.tickerAnchor} to={`/stocks/${ticker}`}>{ticker}</Link>
     </span>
   )
 }
