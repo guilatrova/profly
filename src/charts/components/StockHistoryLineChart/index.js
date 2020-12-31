fa3ae1fb36653562b8f9b27c758d9acea1f49ced
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-30 23:32:44 -0300

    style: make chart responsible

--- src/charts/components/StockHistoryLineChart/index.js
+++ src/charts/components/StockHistoryLineChart/index.js
@@ -1,55 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 import { epochToDateOutput } from '../../../utils/dates';
-import { getCurrencyFormattedNumber, getCurrencyRoundedNumber } from '../../../utils/numberFormat';
+import {
+  getCurrencyFormattedNumber,
+  getCurrencyRoundedNumber,
+} from '../../../utils/numberFormat';
 import TransactionsTooltip from './TransactionsTooltip';
 import DateAxisTick from './DateAxisTick';
 import TransactionDot from './TransactionDot';
 
 const StockHistoryLineChart = ({ chartData }) => {
   return (
-    <LineChart
-      width={1000}
-      height={300}
-      data={chartData.data}
-      margin={{
-        top: 5, right: 30, left: 20, bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
+    <ResponsiveContainer height={300} width="100%">
+      <LineChart
+        data={chartData.data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
 
-      <XAxis
-        dataKey="date"
-        domain={chartData.xDomain}
-        scale="time"
-        type="number"
-        tick={DateAxisTick}
-      />
-      <YAxis
-        tickFormatter={getCurrencyRoundedNumber}
-        domain={chartData.yDomain}
-      />
+        <XAxis
+          dataKey="date"
+          domain={chartData.xDomain}
+          scale="time"
+          type="number"
+          tick={DateAxisTick}
+        />
+        <YAxis
+          tickFormatter={getCurrencyRoundedNumber}
+          domain={chartData.yDomain}
+        />
 
-      <Tooltip
-        content={<TransactionsTooltip />}
-        labelFormatter={epochToDateOutput}
-        formatter={getCurrencyFormattedNumber}
-      />
-      <Legend />
+        <Tooltip
+          content={<TransactionsTooltip />}
+          labelFormatter={epochToDateOutput}
+          formatter={getCurrencyFormattedNumber}
+        />
+        <Legend />
 
-      <Line type="monotone" dataKey="close" stroke="#8884d8" dot={<TransactionDot />} />
-      {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
-    </LineChart>
+        <Line
+          type="monotone"
+          dataKey="close"
+          stroke="#8884d8"
+          dot={<TransactionDot />}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
-}
+};
 
 StockHistoryLineChart.propTypes = {
   chartData: PropTypes.shape({
     data: PropTypes.array,
     xDomain: PropTypes.arrayOf(PropTypes.number),
-    yDomain: PropTypes.arrayOf(PropTypes.number)
-  })
-}
+    yDomain: PropTypes.arrayOf(PropTypes.number),
+  }),
+};
 
 export default StockHistoryLineChart;
