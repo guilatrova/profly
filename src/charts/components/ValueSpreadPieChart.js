0d99a45e80a367df7c67798c47f68b3a82c37649
Guilherme Latrova <guilhermelatrova@hotmail.com>
2020-12-31 13:26:46 -0300

    feat: improve pie chart feel

--- src/charts/components/ValueSpreadPieChart.js
+++ src/charts/components/ValueSpreadPieChart.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { PieChart, Pie, Tooltip, Cell, Sector } from 'recharts';
 import { randomColor } from '../../utils/colors';
-
+import ActiveShape from './ValueSpreadActiveShape';
 
 const ValueSpreadPieChart = ({ chartData }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {
+    if (chartData.length !== colors.length) {
+      setColors(chartData.map(() => randomColor()));
+    }
+  }, [chartData]);
+
   return (
-    <PieChart width={300} height={250}>
+    <PieChart width={500} height={250}>
       <Pie
+        activeIndex={activeIndex}
+        activeShape={ActiveShape}
         data={chartData}
         cx={150}
         cy={120}
         innerRadius={60}
         outerRadius={80}
         paddingAngle={5}
-        label
+        onMouseEnter={(data, idx) => setActiveIndex(idx)}
+        onMouseLeave={() => setActiveIndex()}
       >
-        {chartData.map((entry, index) =>
-          <Cell key={`cell-${index}`} fill={randomColor()} />)}
+        {chartData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index]} />
+        ))}
       </Pie>
 
       <Tooltip />
@@ -26,7 +39,7 @@ const ValueSpreadPieChart = ({ chartData }) => {
 };
 
 ValueSpreadPieChart.propTypes = {
-  chartData: PropTypes.array.isRequired
+  chartData: PropTypes.array.isRequired,
 };
 
 export default ValueSpreadPieChart;
