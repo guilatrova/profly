a850ef61d4948643726b1d3d8bcc11e63972736b
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-01-03 13:31:17 -0300

    feat: add authorization header

--- src/core/apollo/index.js
+++ src/core/apollo/index.js
@@ -1,8 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getSessionToken } from '../authentication';
+
+const getAuthorizationHeader = async () => ({ Authorization: `Bearer ${await getSessionToken()}` });
+
+const getHeaders = async () => {
+  const headers = await getAuthorizationHeader();
+
+  return { headers };
+};
+
+const authMiddleware = setContext(getHeaders);
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8000/graphql/'
+});
+
+const link = ApolloLink.from([ authMiddleware, httpLink ]);
 
 const client = new ApolloClient({
   uri: 'http://localhost:8000/graphql/',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link
 });
 
 export default client;
