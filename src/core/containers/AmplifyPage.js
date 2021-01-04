6d3c4d65cd65658c77b7d6e77dd57609c6dbde80
Guilherme Latrova <guilhermelatrova@hotmail.com>
2021-01-04 16:05:02 -0300

    feat: remove phone field from sign up

--- src/core/containers/AmplifyPage.js
+++ src/core/containers/AmplifyPage.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import {
-  AmplifyAuthenticator,
-  AmplifySignIn,
-  AmplifySignUp,
-} from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react';
 import { Redirect } from 'react-router-dom';
 import { useAuthentication } from '../authentication';
 
+const FORM_FIELDS = [
+  { type: 'username' },
+  { type: 'email' },
+  { type: 'password' },
+];
+
 const AmplifyPage = () => {
   const { isAuthenticated, refresh } = useAuthentication();
-  const formFields = [
-    { type: 'username' },
-    { type: 'email' },
-    { type: 'password' },
-  ];
 
-  const handleAuthStateChange = () => refresh();
-
-  if (isAuthenticated) return <Redirect to={{ pathname: "/" }} />;
+  if (isAuthenticated) return <Redirect to={{ pathname: '/' }} />;
 
   return (
-    <AmplifyAuthenticator>
-      <AmplifySignIn
-        handleAuthStateChange={handleAuthStateChange}
-        slot="sign-in"
-      ></AmplifySignIn>
-
-      <AmplifySignUp slot="sign-up" formFields={formFields}></AmplifySignUp>
+    <AmplifyAuthenticator handleAuthStateChange={refresh}>
+      <AmplifySignUp slot="sign-up" formFields={FORM_FIELDS}></AmplifySignUp>
     </AmplifyAuthenticator>
   );
 };
