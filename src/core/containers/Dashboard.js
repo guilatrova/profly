5ec1c121e5b53c39056cb60efc427bfec90baf49
Guilherme Latrova <hello@guilatrova.dev>
2021-12-03 10:00:19 -0300

    refactor: move components

--- src/core/containers/Dashboard.js
+++ src/core/containers/Dashboard.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import RecentTransactions from '../../transactions/containers/RecentTransactions';
 import StocksSummary from './StocksSummary';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -21,6 +22,8 @@ const Dashboard = () => {
     <Grid container spacing={3}>
 
       <Grid item xs={12}>
+        <Typography component="h2" variant="h6">Stocks</Typography>
+
         <StocksSummary />
       </Grid>
 
--- src/core/containers/StocksSummary.js
+++ src/core/containers/StocksSummary.js
@@ -4,8 +4,6 @@ import { useQuery } from '@apollo/client';
 import queries from '../../charts/queries';
 import ErrorHandler from '../../core/components/ApolloErrorHandler';
 import StocksTable from '../../stocks/components/StocksTable';
-import Typography from '@material-ui/core/Typography';
-
 
 const StocksSummary = () => {
   const { loading, error, data = [] } = useQuery(queries.chartStocksValues);
@@ -16,8 +14,6 @@ const StocksSummary = () => {
     <Grid container spacing={3}>
 
       <Grid item xs={12} md={6}>
-        <Typography component="h2" variant="h6">Stocks</Typography>
-
         <StocksTable data={data?.stocks} loading={loading} />
       </Grid>
 
