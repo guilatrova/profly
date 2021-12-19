74a7bacd14d183955bbc4d20e3f2715c287707d6
Guilherme Latrova <hello@guilatrova.dev>
2021-12-19 11:11:06 -0300

    fix: set correct api endpoint

--- src/core/constants/api.js
+++ src/core/constants/api.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "http://localhost:8000";
-// export const API_ENDPOINT = "https://api.profly.app"
+// export const API_ENDPOINT = "http://localhost:8000";
+export const API_ENDPOINT = 'https://api.profly.app'
