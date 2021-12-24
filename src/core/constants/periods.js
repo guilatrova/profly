0eceddd3790b707ceda98a5b6a97c3bc2e425703
Guilherme Latrova <hello@guilatrova.dev>
2021-12-24 19:16:29 -0300

    feat: remove 5y from periods

--- src/core/constants/periods.js
+++ src/core/constants/periods.js
@@ -1,11 +1,11 @@
 export default [
-  {display: 'd', value: '1d'},
-  {display: 'w', value: '5d'},
-  {display: '1m', value: '1mo'},
-  {display: '3m', value: '3mo'},
-  {display: '6m', value: '6mo'},
-  {display: 'y', value: 'ytd'},
-  {display: '2y', value: '2y'},
-  {display: '5y', value: '5y'},
-  {display: 'all', value: 'max'},
-];
+  { display: 'd', value: '1d' },
+  { display: 'w', value: '5d' },
+  { display: '1m', value: '1mo' },
+  { display: '3m', value: '3mo' },
+  { display: '6m', value: '6mo' },
+  { display: 'y', value: 'ytd' },
+  { display: '2y', value: '2y' },
+  // {display: '5y', value: '5y'},
+  { display: 'all', value: 'max' },
+]
