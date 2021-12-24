d6df3011c785f882e8c05099188c25dcceb8056d
Guilherme Latrova <hello@guilatrova.dev>
2021-12-24 19:16:41 -0300

    style: lint

--- src/core/components/LoggedUser.js
+++ src/core/components/LoggedUser.js
@@ -1,53 +1,56 @@
-import React, { useEffect,useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react'
 
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import Button from '@material-ui/core/Button'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
+import Grow from '@material-ui/core/Grow'
+import MenuItem from '@material-ui/core/MenuItem'
+import MenuList from '@material-ui/core/MenuList'
+import Paper from '@material-ui/core/Paper'
+import Popper from '@material-ui/core/Popper'
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown'
 
-import { useAuthentication } from '../authentication';
-
+import { useAuthentication } from '../authentication'
 
 const LoggedUser = () => {
-  const [open, setOpen] = useState(false);
-  const { isAuthenticated, logout, user } = useAuthentication();
-  const anchorRef = useRef(null);
+  const [open, setOpen] = useState(false)
+  const { isAuthenticated, logout, user } = useAuthentication()
+  const anchorRef = useRef(null)
 
-  const handleToggle = () => setOpen((prevOpen) => !prevOpen);
+  const handleToggle = () => setOpen((prevOpen) => !prevOpen)
   const handleClose = (event) => {
-    if (event && anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
+    if (
+      event &&
+      anchorRef.current &&
+      anchorRef.current.contains(event.target)
+    ) {
+      return
     }
 
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   const handleListKeyDown = (event) => {
     if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
+      event.preventDefault()
+      setOpen(false)
     }
   }
 
   const handleLogout = () => {
-    logout();
-    handleClose();
-  };
+    logout()
+    handleClose()
+  }
 
   // return focus to the button when we transitioned from !open -> open
-  const prevOpen = React.useRef(open);
+  const prevOpen = React.useRef(open)
   useEffect(() => {
     if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
+      anchorRef.current.focus()
     }
-    prevOpen.current = open;
-  }, [open]);
+    prevOpen.current = open
+  }, [open])
 
-  if (!isAuthenticated) return <></>;
+  if (!isAuthenticated) return <></>
 
   return (
     <div>
@@ -92,7 +95,7 @@ const LoggedUser = () => {
         )}
       </Popper>
     </div>
-  );
-};
+  )
+}
 
-export default LoggedUser;
+export default LoggedUser
