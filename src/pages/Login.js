6fc69a3368e289458c2a1f99edcb046d4605bf86
Guilherme Latrova <hello@guilatrova.dev>
2021-12-29 20:43:30 -0300

    feat: add proper messaging for amplify form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 
+import { Hub } from '@aws-amplify/core'
+import {
+  TOAST_AUTH_ERROR_EVENT,
+  UI_AUTH_CHANNEL,
+} from '@aws-amplify/ui-components'
 import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react'
 import { useAuthentication } from 'core/authentication'
+import { useSnackbar } from 'notistack'
 
 const FORM_FIELDS = [
   { type: 'username' },
@@ -12,11 +18,23 @@ const FORM_FIELDS = [
 
 const AmplifyPage = () => {
   const { isAuthenticated, refresh } = useAuthentication()
+  const { enqueueSnackbar } = useSnackbar()
+
+  const handleToastErrors = ({ payload }) => {
+    if (payload.event === TOAST_AUTH_ERROR_EVENT && payload.message) {
+      enqueueSnackbar(payload.message, { variant: 'error' })
+    }
+  }
+
+  useEffect(() => {
+    Hub.listen(UI_AUTH_CHANNEL, handleToastErrors)
+    return () => Hub.remove(UI_AUTH_CHANNEL, handleToastErrors)
+  })
 
   if (isAuthenticated) return <Redirect to={{ pathname: '/' }} />
 
   return (
-    <AmplifyAuthenticator handleAuthStateChange={refresh}>
+    <AmplifyAuthenticator hideToast handleAuthStateChange={refresh}>
       <AmplifySignUp formFields={FORM_FIELDS} slot="sign-up"></AmplifySignUp>
     </AmplifyAuthenticator>
   )
