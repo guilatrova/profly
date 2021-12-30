cd21a87a17f2fb1942215e9bcef4afdf23ad68ec
Guilherme Latrova <hello@guilatrova.dev>
2021-12-30 17:28:14 -0300

    style: lint

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 /* eslint-disable import/default */
 
-import React from 'react';
-import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
-import configureStore, { history } from './core/store/configureStore';
-import Root from './Root';
-require('./favicon.png'); // Tell webpack to load favicon.ico
-const store = configureStore();
+import configureStore, { history } from './core/store/configureStore'
+import Root from './Root'
+require('./favicon.png') // Tell webpack to load favicon.ico
+const store = configureStore()
 
 render(
   <AppContainer>
     <Root history={history} store={store} />
   </AppContainer>,
   document.getElementById('app')
-);
+)
 
 if (module.hot) {
   module.hot.accept('./Root', () => {
-    const NewRoot = require('./Root').default;
+    const NewRoot = require('./Root').default
     render(
       <AppContainer>
         <NewRoot history={history} store={store} />
       </AppContainer>,
       document.getElementById('app')
-    );
-  });
+    )
+  })
 }
