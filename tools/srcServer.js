03087b5375d5710fecf35794b81a8dcb537d2902
Guilherme Latrova <hello@guilatrova.dev>
2021-12-30 18:11:22 -0300

    style: lint

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -2,49 +2,41 @@
 // which supports hot reloading and synchronized testing.
 
 // Require Browsersync along with webpack and middleware for it
-import browserSync from 'browser-sync';
+import browserSync from 'browser-sync'
 // Required for react-router browserHistory
 // see https://github.com/BrowserSync/browser-sync/issues/204#issuecomment-102623643
-import historyApiFallback from 'connect-history-api-fallback';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
+import historyApiFallback from 'connect-history-api-fallback'
+import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 
-import config from '../webpack.config.dev';
+import config from '../webpack.config.dev'
 
-const bundler = webpack(config);
+const bundler = webpack(config)
 
 // Run Browsersync and use middleware for Hot Module Replacement
 browserSync({
   // no need to watch '*.js' here, webpack will take care of it for us,
-// including full page reloads if HMR won't work
-files: [
-    'src/*.html'
-  ],
-  
+  // including full page reloads if HMR won't work
+  files: ['src/*.html'],
 
-port: 3000,
-  
+  port: 3000,
 
-server: {
+  server: {
     baseDir: 'src',
 
     middleware: [
-      historyApiFallback(
-        {
-          disableDotRule: true,
-          htmlAcceptHeaders: ['text/html', 'application/xhtml+xml']
-        }
-      ),
+      historyApiFallback({
+        disableDotRule: true,
+        htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'],
+      }),
 
       webpackDevMiddleware(bundler, {
-        
         // These settings suppress noisy webpack output so only errors are displayed to the console.
-noInfo: true,
+        noInfo: true,
 
-        
         // Dev middleware can't access config, so we provide publicPath
-publicPath: config.output.publicPath,
+        publicPath: config.output.publicPath,
         quiet: false,
         stats: {
           assets: false,
@@ -53,7 +45,7 @@ publicPath: config.output.publicPath,
           colors: true,
           hash: false,
           timings: false,
-          version: false
+          version: false,
         },
 
         // for other settings see
@@ -61,13 +53,11 @@ publicPath: config.output.publicPath,
       }),
 
       // bundler should be the same as above
-      webpackHotMiddleware(bundler)
-    ]
+      webpackHotMiddleware(bundler),
+    ],
   },
 
-  
-  
   ui: {
-    port: 3001
-  }
-});
+    port: 3001,
+  },
+})
